44c6275ebfac8c9e970440fdf75ac2e2321f17d9
Ruihua Sui <rsui2@illinois.edu>
2019-05-29 04:28:47 -0500

    Updates

---
+++ precache-manifest.518ef12a2ab7f7ca7f13316a6ec4d526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28b5600d93202f0b77f1dcf49f8a996",
+    "url": "/react-gobang/index.html"
+  },
+  {
+    "revision": "39f67d203e2eed2dfe77",
+    "url": "/react-gobang/static/css/main.51474506.chunk.css"
+  },
+  {
+    "revision": "1d43d8f07a8ca762564b",
+    "url": "/react-gobang/static/js/2.6c4acb8c.chunk.js"
+  },
+  {
+    "revision": "39f67d203e2eed2dfe77",
+    "url": "/react-gobang/static/js/main.ee7d2dee.chunk.js"
+  },
+  {
+    "revision": "707bbb60d65688b688f2",
+    "url": "/react-gobang/static/js/runtime~main.7f7ca634.js"
+  }
+]);
