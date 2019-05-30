77e1978fb4fe83bfb23c9dea6fd3e618d05d8970
Ruihua Sui <rsui2@illinois.edu>
2019-05-29 19:37:56 -0500

    add new features

--- src/App.js
+++ src/App.js
@@ -4,32 +4,24 @@ import GobangPage from './game-gobang/gobangPage.js'
 import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">Gobang</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="/gobang">Gobang</Nav.Link>
-              <Nav.Link href="https://ruihuasui.github.io/react-simple2048/">Simple 2048</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-info">Search</Button>
-            </Form>
-          </Navbar>
-          
-          <Route exact path="/" component={GobangPage} />
-          <Route exact path="/gobang" component={GobangPage} />
-          {/* <Route path="/users" component={Simple2048Page} /> */}
-        </div>
-      </Router>
-    );
-  }
+export default function App (){
+  return (
+    <Router>
+      <div>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="/react-gobang/">Gobang</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/react-gobang/">Gobang</Nav.Link>
+            <Nav.Link href="https://ruihuasui.github.io/react-simple2048/">Simple 2048</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+        
+        <Route exact path="/react-gobang/" component={GobangPage} />
+      </div>
+    </Router>
+  );
 }
