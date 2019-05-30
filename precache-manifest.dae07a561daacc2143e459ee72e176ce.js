788854639a9890c4afe10ea22ced14ff1a281259
Ruihua Sui <rsui2@illinois.edu>
2019-05-29 19:14:25 -0500

    Updates

--- precache-manifest.dae07a561daacc2143e459ee72e176ce.js
+++ precache-manifest.dae07a561daacc2143e459ee72e176ce.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "26bbbebbf083b03411e67813c2abd59d",
+    "revision": "a453ccc671ccc8730063ba1e08ae653a",
     "url": "/react-gobang/index.html"
   },
   {
-    "revision": "ce6578a2c905874d1ff2",
+    "revision": "7013ba373a48a2fe0b43",
     "url": "/react-gobang/static/css/main.10c04e45.chunk.css"
   },
   {
-    "revision": "ecdba074d86004c68b58",
-    "url": "/react-gobang/static/js/2.d0a429b8.chunk.js"
+    "revision": "5c321f65d7e87bf91be1",
+    "url": "/react-gobang/static/js/2.2275f1a6.chunk.js"
   },
   {
-    "revision": "ce6578a2c905874d1ff2",
-    "url": "/react-gobang/static/js/main.64ce235d.chunk.js"
+    "revision": "7013ba373a48a2fe0b43",
+    "url": "/react-gobang/static/js/main.8ede293e.chunk.js"
   },
   {
     "revision": "707bbb60d65688b688f2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gobang/precache-manifest.d2a1ba638d3646e492043557d0b5a81e.js"
+  "/react-gobang/precache-manifest.dae07a561daacc2143e459ee72e176ce.js"
 );
 
 self.addEventListener('message', (event) => {
