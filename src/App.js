12cb7d2615b6943523530cd9b6df0a143d7d5105
Ruihua Sui <rsui2@illinois.edu>
2019-05-30 01:30:18 -0500

    new features

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App (){
   return (
     <Router>
       <div>
-        <Navbar bg="light" variant="light" style={{zIndex: 1, boxShadow: '5px 5px 15px black'}}>
+        <Navbar bg="light" variant="light" style={{zIndex: 1, boxShadow: '5px 5px 15px rgba(71, 71, 71, 0.931)'}}>
           <Navbar.Brand href="/react-gobang/">
             <img
               className="logo"
